b0ed643bb0ad914300238fe05b5e61c0f941561e
SirawichDev <sirawit0676@gmail.com>
2018-04-15 20:41:38 +0700

    added client id & secret

---
+++ js/gh.js
@@ -0,0 +1,7 @@
+class GH{
+    constructor(){
+        //เอาapi github keyมา
+        this.mines_id = '857bc73c0be5fd78ce8d';
+        this.mines_secret = '775e55cfc92c23959646dad9f9b29895ab7b8e88';
+    }
+}
