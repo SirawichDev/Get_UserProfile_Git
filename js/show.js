1b20026852392cf629940e529987365455520e80
SirawichDev <sirawit0676@gmail.com>
2018-04-16 22:07:16 +0700

    show user.avatar

--- js/show.js
+++ js/show.js
@@ -2,4 +2,29 @@ class show {
     constructor(){
         this.profile = document.getElementById('show');
     }
+    
+    showUser(user){
+        this.profile.innerHTML = `
+        
+        <div class="row">
+            <div class="col-1-of-2">
+                <img src ="${user.avatar_url}"
+            </div>
+            <div class="col-1-of-2">
+                    1 of 2
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1-of-3">
+                    1 of 3 
+            </div>
+            <div class="col-1-of-3">
+                    1 of 3 
+            </div>
+            <div class="col-1-of-3">
+                    1 of 3 
+            </div>
+        </div>
+        `
+    }
 }
